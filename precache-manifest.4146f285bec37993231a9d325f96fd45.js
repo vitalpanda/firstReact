f2196582a1781812cbc02bcffda4d7b384ab8739
vitalpanda <vitalpandamail@gmail.com>
2019-05-28 06:46:10 +0600

    Updates

---
+++ precache-manifest.4146f285bec37993231a9d325f96fd45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352fd6091c9a487970fd4fd8e7d6b88b",
+    "url": "/firstReact/index.html"
+  },
+  {
+    "revision": "d7b4cfed80bda2f07365",
+    "url": "/firstReact/static/css/main.b98c377a.chunk.css"
+  },
+  {
+    "revision": "734c948d99fcc657a2a0",
+    "url": "/firstReact/static/js/2.00399540.chunk.js"
+  },
+  {
+    "revision": "d7b4cfed80bda2f07365",
+    "url": "/firstReact/static/js/main.a27d6d0f.chunk.js"
+  },
+  {
+    "revision": "745e129d1b65c2810402",
+    "url": "/firstReact/static/js/runtime~main.ad412271.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/firstReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/firstReact/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/firstReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/firstReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/firstReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
